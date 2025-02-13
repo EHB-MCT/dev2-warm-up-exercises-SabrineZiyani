f5a98bb4c7454ef1c7f9e9935dffb1517b8eafd1
Sabrine Ziyani <sabrine.ziyani@student.ehb.be>
2025-02-13 11:42:50 +0100

    Change text

--- main.js
+++ main.js
@@ -5,9 +5,22 @@ import {
 } from './lib.js';
 
 runTitle();
-
+warmup1();
 function warmup1() {
     console.log("Exercise week 1");
+
+
+    const textnaam = document.querySelector("#content-1 p");
+    const button = document.querySelector("#button-1a");
+    button.addEventListener('click', function () {
+        textnaam.textContent = "Hallo, dit is de oefening van Sabrine";
+    });
+
+
+    const button = document.querySelector("#button-1b");
+
+
+
 }
 
 function warmup2() {
